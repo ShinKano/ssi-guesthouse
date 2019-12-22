d5c64f17848fc30e34b1ed356c6993d1f1d7ddf1
kanoshinnosuke <yushph@gmail.com>
2019-12-22 10:15:18 +0900

    add og-url

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
+        <meta property="og:url" content="https://www.ss-guesthouse.com/" />
         <meta
           property="og:image"
           content={`${withPrefix('/')}img/og-image.png`}
