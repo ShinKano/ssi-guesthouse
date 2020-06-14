3145a269f69309897c502fbbcfc8c04d9165b104
kanoshinnosuke <yushph@gmail.com>
2020-06-14 09:43:27 +0900

    delete apas

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,6 +7,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/src/templates/blog-post.js"))),
   "component---src-templates-tags-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/src/templates/tags.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/src/pages/404.js"))),
   "component---src-pages-blog-index-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/src/pages/blog/index.js"))),
   "component---src-pages-calender-2020-feb-js": hot(preferDefault(require("/Users/Shinnosuke/Documents/batch5/ssi-guesthouse/src/pages/calender/2020/Feb.js"))),
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -4,9 +4,6 @@ import { useIntl, FormattedMessage } from "gatsby-plugin-intl"
 import Button from '../elements/Button'
 import Gallery_1_2 from '../elements/Gallery_1_2'
 //
-import apas_out from '../img/apas_out.jpg'
-import apas_single001 from '../img/apas_single001.jpg'
-import apas_kitchen from '../img/apas_kitchen.jpg'
 import guadalupe_out from '../img/guadalupe_out.jpg'
 import guadalupe_single002 from '../img/guadalupe_single002.jpg'
 import guadalupe_living001 from '../img/guadalupe_living001.jpg'
@@ -27,35 +24,6 @@ const Locations = () => {
                         <FormattedMessage id="locations02" />
                     </span>
                 </h2>
-                <div className="columns margin-top-2">
-                    <div className="column max-width-1">
-                        <h3 className="title is-4 has-text-centered">
-                            <FormattedMessage id="cebu-apas" />
-                        </h3>
-                        <p>
-                            <FormattedMessage id="apas-text01" />
-                        </p>
-                        <p className="margin-top">
-                            <FormattedMessage id="apas-text02" />
-                        </p>
-                        <p className="margin-top">
-                            <FormattedMessage id="apas-text03" />
-                        </p>
-                        <div className="has-text-centered margin-top-2">
-                            <Button
-                                to="/rooms#apas"
-                                text={intl.formatMessage({ id: "button-rooms" })}
-                            />
-                        </div>
-                    </div>
-                    <div className="column is-6">
-                        <Gallery_1_2
-                            img1={apas_out}
-                            img2={apas_single001}
-                            img3={apas_kitchen}
-                        />
-                    </div>
-                </div>
 
                 <div className="margin-top-2" />
 
